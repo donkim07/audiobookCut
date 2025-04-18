9eb877de3b23de9439f39bde85b76c67a45762fe
Donald <donestomih@gmail.com>
2025-04-18 04:59:52 +0300

    Update project configuration and dependencies: rename package to 'audiobookcut', update Dart SDK and Flutter versions, add Linux and macOS support, and enhance .gitignore for build artifacts.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:audiobook_cut/main.dart';
+import 'package:audiobookcut/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
